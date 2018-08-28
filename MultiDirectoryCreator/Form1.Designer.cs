a47393c35789fba3356d117a5a79fff13d589669
Adam Marcionek <amarcionek@seven10storage.com>
2018-08-28 11:00:33 -0400

    Initial version
    Supports creation of 4 levels of directories across N threads, with the
    bottom most level being a GUID.  All n threads are synchronized to
    execute simultaneously. This is to ensure that no thread gets ahead of
    others in the naming scheme.

---
+++ MultiDirectoryCreator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MultiDirectoryCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.threads = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.BaseDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.rootPath = new System.Windows.Forms.TextBox();
+            this.labelThreads = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.threads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // threads
+            // 
+            this.threads.Location = new System.Drawing.Point(103, 27);
+            this.threads.Name = "threads";
+            this.threads.Size = new System.Drawing.Size(61, 20);
+            this.threads.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 129);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(12, 61);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.BtnBrowse.TabIndex = 2;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // rootPath
+            // 
+            this.rootPath.Location = new System.Drawing.Point(103, 63);
+            this.rootPath.Name = "rootPath";
+            this.rootPath.Size = new System.Drawing.Size(172, 20);
+            this.rootPath.TabIndex = 3;
+            this.rootPath.TextChanged += new System.EventHandler(this.TextChanged_Click);
+            // 
+            // labelThreads
+            // 
+            this.labelThreads.AutoSize = true;
+            this.labelThreads.Location = new System.Drawing.Point(13, 33);
+            this.labelThreads.Name = "labelThreads";
+            this.labelThreads.Size = new System.Drawing.Size(46, 13);
+            this.labelThreads.TabIndex = 4;
+            this.labelThreads.Text = "Threads";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(103, 129);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 414);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.labelThreads);
+            this.Controls.Add(this.rootPath);
+            this.Controls.Add(this.BtnBrowse);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.threads);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.threads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown threads;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog BaseDir;
+        private System.Windows.Forms.TextBox rootPath;
+        private System.Windows.Forms.Label labelThreads;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ MultiDirectoryCreator/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiDirectoryCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rootPath.Text = baseDirectory;
+            threads.Value = 1;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+             w = File.AppendText("log.txt");
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            if (running == true)
+                return;
+
+            running = true;
+            testDir = 0;
+            runThread = new System.Threading.Thread(new System.Threading.ThreadStart(runTasks));
+            runThread.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            if (running == false)
+                return;
+
+            running = false;
+            runThread.Abort();
+        }
+
+        private void runTasks()
+        {
+            while (true)
+            {
+                List<Task> TaskList = new List<Task>();
+
+                for (int i = 0; i < this.threads.Value; i++)
+                {                    
+                    var LastTask = new Task(process);
+                    LastTask.Start();
+                    TaskList.Add(LastTask);
+                }
+
+                Task.WaitAll(TaskList.ToArray());
+                testDir++;
+             }
+        }
+
+        private void process()
+        {
+            string dirCreate = baseDirectory + @"\" + testDir.ToString() + @"\" + testDir.ToString() + @"\" + testDir.ToString() + @"\" + testDir.ToString() + @"\" + Guid.NewGuid().ToString();
+            try
+            {
+                DirectoryInfo dInfo = Directory.CreateDirectory(dirCreate);
+                Console.WriteLine("Directory info: {0} {1}", dirCreate, dInfo.Attributes.ToString());
+            }
+            catch (ThreadInterruptedException ex)
+            {
+                Console.WriteLine("Interrupted: {0}", ex.Message);
+                Log("Interrupted", w);
+            }
+            catch (ThreadAbortException ex)
+            {
+                Console.WriteLine("Aborted: {0}", ex.Message);
+                Log("Aborted", w);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Caught IOException: {0}", ex.Message);
+                Log(ex.Message, w);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught Exception: {0}", ex.Message);
+                Log(ex.Message, w);
+            }
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = BaseDir.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                baseDirectory = BaseDir.SelectedPath;
+                rootPath.Text = baseDirectory;
+            }
+        }
+
+        private void TextChanged_Click(object sender, EventArgs e)
+        {
+            baseDirectory = rootPath.Text;
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.WriteLine("\nLog Entry : {0} {1} {2} {3}",
+                DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString(),
+                logMessage,
+                "-------------------------------");
+            w.Flush();
+        }
+
+        private string baseDirectory = "C:\\TEMP";
+        static private System.Threading.Thread runThread;
+        private Boolean running = false;
+        private StreamWriter w;
+        private int testDir = 0;
+    }
+}
